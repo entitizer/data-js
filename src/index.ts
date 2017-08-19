f22b55d5b21abbbdc6b1291bea2c0581bf5fa303
Dumitru Cantea <canteadumitru@gmail.com>
2017-08-19 15:57:37 +0300

    DataEntityUniqueNameRepository

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { EntityDataMapper, EntityUniqueNameDataMapper } from './entities';
 export * from './storage';
 export { DataEntityRepository } from './entity-repository';
+export { DataEntityUniqueNameRepository } from './entity-uniquename-repository';
