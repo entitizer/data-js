d2c5191d2f87c56929b4d69976adaff43c3bd6cf
Dumitru Cantea <canteadumitru@gmail.com>
2017-08-20 14:26:51 +0300

    fix .store

--- src/uniquename-repository.ts
+++ src/uniquename-repository.ts
@@ -16,15 +16,15 @@ export class DataUniqueNameRepository implements UniqueNameRepository {
         return this.store.update(data).map(item => this.mapper.toDomain(item));
     }
     getById(id: UniqueNameID, options?: RepAccessOptions): Observable<UniqueName> {
-        return this.getById(id).map(item => this.mapper.toDomain(item));
+        return this.store.getById(id).map(item => this.mapper.toDomain(item));
     }
     getByIds(ids: UniqueNameID[], options?: RepAccessOptions): Observable<UniqueName[]> {
         return this.store.getByIds(ids).map(items => items.map(item => this.mapper.toDomain(item)));
     }
     delete(id: UniqueNameID): Observable<UniqueName> {
-        return this.delete(id).map(item => this.mapper.toDomain(item));
+        return this.store.delete(id).map(item => this.mapper.toDomain(item));
     }
     create(data: UniqueName, options?: RepAccessOptions): Observable<UniqueName> {
-        return this.create(data).map(item => this.mapper.toDomain(item));
+        return this.store.create(data).map(item => this.mapper.toDomain(item));
     }
 }
